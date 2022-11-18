664df367e6d0921fa156d08fc5f8f83392f18e3e
Giovanni De Vanno <giovannidv96@gmail.com>
2022-11-18 16:26:30 +0100

    implementazione layout FirstActivity

--- app/src/main/java/com/unisannio/gdevanno/intentexamples/FirstActivity.java
+++ app/src/main/java/com/unisannio/gdevanno/intentexamples/FirstActivity.java
@@ -3,12 +3,17 @@ package com.unisannio.gdevanno.intentexamples;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class FirstActivity extends AppCompatActivity {
+
+    private EditText edit;
+    private final int rCode = 1963;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        edit = (EditText) findViewById(R.id.input);
     }
 }
