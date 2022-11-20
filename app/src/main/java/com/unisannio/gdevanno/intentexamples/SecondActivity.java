0041e7b35c35880c20d73546e8fb3cc30b3eba9c
Giovanni De Vanno <giovannidv96@gmail.com>
2022-11-20 12:44:09 +0100

    implementazione SecondActivity

--- app/src/main/java/com/unisannio/gdevanno/intentexamples/SecondActivity.java
+++ app/src/main/java/com/unisannio/gdevanno/intentexamples/SecondActivity.java
@@ -2,7 +2,9 @@ package com.unisannio.gdevanno.intentexamples;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Intent fromCaller = getIntent();
+        int param = fromCaller.getIntExtra(getResources().getString(R.string.key),-1);
+        if(param>-1){
+            TextView tv = (TextView)findViewById(R.id.textView);
+            tv.setText(String.valueOf(param));
+        }
     }
 }
